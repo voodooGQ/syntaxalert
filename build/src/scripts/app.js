87fdfbac23695e2747f6fd69e02d1c3f6616a111
Shane Smith <ssmith@nerdery.com>
2016-12-31 12:49:22 -0600

    Adding prism vim component

--- build/src/scripts/app.js
+++ build/src/scripts/app.js
@@ -12,7 +12,7 @@ Prism['css']		= require('../vendor/prism/components/prism-css');
 Prism['scss']		= require('../vendor/prism/components/prism-scss');
 Prism['sass']		= require('../vendor/prism/components/prism-sass');
 Prism['ruby']		= require('../vendor/prism/components/prism-ruby');
-Prism['vim']        = require('../vendor/prism/component/prism-vim');
+Prism['vim']        = require('../vendor/prism/components/prism-vim');
 
 var SearchFormView = require('./views/SearchForm');
 
