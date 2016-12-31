7c614c53864f307a3a3c1322ba097722efbc6809
Shane Smith <ssmith@nerdery.com>
2016-12-31 12:11:56 -0600

    Adding additional language components, styling bash blocks.

--- build/src/scripts/app.js
+++ build/src/scripts/app.js
@@ -2,8 +2,17 @@
 'use strict';
 
 window.$ = window.jQuery = require('jquery');
-var Prism = require('../vendor/prism/prism');
-var PrismPHP = require('../vendor/prism/components/prism-php');
+
+var Prism = [];
+Prism['core'] 		= require('../vendor/prism/prism');
+Prism['bash'] 		= require('../vendor/prism/components/prism-bash');
+Prism['JSON'] 		= require('../vendor/prism/components/prism-JSON');
+Prism['php']		= require('../vendor/prism/components/prism-php');
+Prism['css']		= require('../vendor/prism/components/prism-css');
+Prism['scss']		= require('../vendor/prism/components/prism-scss');
+Prism['sass']		= require('../vendor/prism/components/prism-sass');
+Prism['ruby']		= require('../vendor/prism/components/prism-ruby');
+
 var SearchFormView = require('./views/SearchForm');
 
 var $searchForm = $('.js-searchFormContainer');
