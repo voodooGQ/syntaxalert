4c110ae7866a183a2b756717420c94fcffa110fd
Shane Smith <ssmith@nerdery.com>
2017-01-01 15:32:02 -0600

    Removing trailing whitespace

--- public/content/themes/oowp/functions.php
+++ public/content/themes/oowp/functions.php
@@ -6,8 +6,6 @@
  * @author Shane Smith <voodoogq@gmail.com>
  * @since  1.0
  */
-
 require_once(get_template_directory() . '/lib/vendor/autoload.php');
-
 // Primary Instantiation
 $setup = \Wp\Theme\Setup::getInstance();
