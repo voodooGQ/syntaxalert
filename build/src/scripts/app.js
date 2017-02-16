a754c7c1c7d234daba672116e88e54fc43a2e7e4
Shane Smith <ssmith@nerdery.com>
2017-02-15 22:16:48 -0600

    Updates to language additions

--- build/src/scripts/app.js
+++ build/src/scripts/app.js
@@ -4,19 +4,21 @@
 window.$ = window.jQuery = require('jquery');
 
 var Prism = [];
-Prism['core'] 		= require('../vendor/prism/prism');
-Prism['bash'] 		= require('../vendor/prism/components/prism-bash');
-Prism['json'] 		= require('../vendor/prism/components/prism-json');
-Prism['php']		= require('../vendor/prism/components/prism-php');
-Prism['css']		= require('../vendor/prism/components/prism-css');
-Prism['scss']		= require('../vendor/prism/components/prism-scss');
-Prism['sass']		= require('../vendor/prism/components/prism-sass');
-Prism['ruby']		= require('../vendor/prism/components/prism-ruby');
+Prism['core']       = require('../vendor/prism/prism');
+Prism['bash']       = require('../vendor/prism/components/prism-bash');
+Prism['json']       = require('../vendor/prism/components/prism-json');
+Prism['php']        = require('../vendor/prism/components/prism-php');
+Prism['css']        = require('../vendor/prism/components/prism-css');
+Prism['scss']       = require('../vendor/prism/components/prism-scss');
+Prism['sass']       = require('../vendor/prism/components/prism-sass');
+Prism['ruby']       = require('../vendor/prism/components/prism-ruby');
 Prism['vim']        = require('../vendor/prism/components/prism-vim');
-Prism['nginx']		= require('../vendor/prism/components/prism-nginx');
+Prism['nginx']      = require('../vendor/prism/components/prism-nginx');
+Prism['yaml']       = require('../vendor/prism/components/prism-nginx');
+
 var SearchFormView = require('./views/SearchForm');
 
 var $searchForm = $('.js-searchFormContainer');
 if($searchForm.length > 0) {
-	var $searchForm = new SearchFormView($('.js-searchFormContainer'));
+    var $searchForm = new SearchFormView($('.js-searchFormContainer'));
 }
